aa81cbb8612b0e39b142dba450ebfb0c23c66612
Felipe Faria <felipe.a.b.faria@gmail.com>
2019-06-05 15:03:09 -0300

    Fixed code to return the intersected node

--- src/br/com/github/disparter/study/linkedlists/LinkedListIntersectionChecker.java
+++ src/br/com/github/disparter/study/linkedlists/LinkedListIntersectionChecker.java
@@ -2,30 +2,55 @@ package br.com.github.disparter.study.linkedlists;
 
 public class LinkedListIntersectionChecker {
 	
-	public static Boolean check(LinkedListNode node1, LinkedListNode node2) {
-		while(node1.next != null) {
-			if(node1.equals(node2)) {
-				return Boolean.TRUE;
-			}
-			node1 = node1.next;
+	public static LinkedListNode check(LinkedListNode node1, LinkedListNode node2) {
+		LinkedListNode buff1 = new LinkedListNode(node1.value, node1.next);
+		LinkedListNode buff2 = new LinkedListNode(node2.value, node2.next);
+		Integer node1Length = 0;
+		Integer node2Length = 0;
+		
+		while(buff1.next != null) {
+			buff1 = buff1.next;
+			node1Length++;
 		}
 		
-		while(node2.next != null) {
-			if(node2.equals(node1)) {
-				return Boolean.TRUE;
+		while(buff2.next != null) {
+			buff2 = buff2.next;
+			node2Length++;
+		}
+		
+		if(!buff2.equals(buff1)) {
+			return null;
+		}
+		
+		if(node1Length > node2Length) {
+			int excess = node1Length - node2Length;
+			for(int i = 0; i < excess; i++) {
+				node1 = node1.next;
 			}
+		}else {
+			int excess = node2Length - node1Length;
+			for(int i = 0; i < excess; i++) {
+				node2 = node2.next;
+			}
+		}
+		
+		while(node1.next != null) {
+			if(node1.equals(node2)) {
+				return node1;
+			}
+			node1 = node1.next;
 			node2 = node2.next;
 		}
 		
-		return node1.equals(node2);
-		
+		return null;
 	}
+
 
 	public static void main(String[] args) {
 		LinkedListNode l1 = LinkedListFiller.randomFulfillIntegers(100);
 		LinkedListNode l2 = createIntersectedList(l1);
 		LinkedListNode l3 = LinkedListFiller.randomFulfillIntegers(100);
-		Boolean result = null;
+		LinkedListNode result = null;
 		
 		long start = System.currentTimeMillis();
 		result = LinkedListIntersectionChecker.check(l1, l2);
